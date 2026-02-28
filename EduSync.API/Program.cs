d7b73fdc0c849a67016fdb1d12b1989dec57ee60
Gabriel Fernando Correa <gabrielfcorrea3@gmail.com>
2026-02-28 20:16:39 -0300

    agrego url de backend

--- EduSync.API/Program.cs
+++ EduSync.API/Program.cs
@@ -89,7 +89,9 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngular",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200")
+            policy.WithOrigins("http://localhost:4200",
+                "https://edusync-frontend.onrender.com")
+            
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
